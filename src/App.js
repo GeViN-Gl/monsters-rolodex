39e467fbacfc500cbcdd88c9aad7b2e344062056
GeViN-Gl <gevin.gl@gmail.com>
2021-11-10 21:46:07 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CardList } from "./components/card-list/card-list.component";
+import { Searchbox } from "./components/search-box/search-box.component";
+
+// import logo from "./logo.svg";
+import "./App.scss";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+    // this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    const getMonsters = async (url) => {
+      try {
+        const responce = await fetch(url);
+        if (!responce.ok) return new Error(`fetch fail `, responce.status);
+        const data = await responce.json();
+
+        this.setState({ monsters: data });
+      } catch (err) {
+        console.error(`Fecth fail: `, err);
+      }
+    };
+
+    getMonsters("https://jsonplaceholder.typicode.com/users");
+
+    // fetch("https://jsonplaceholder.typicode.com/users")
+    //   .then((responce) => responce.json())
+    //   .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+
+    const filteredMonster = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1 className="header">Monster Rolodex</h1>
+        <Searchbox placeholder="search monsters" handleChange={this.handleChange} />
+        <CardList monsters={filteredMonster} />
+      </div>
+    );
+  }
 }
 
 export default App;
